b767d5d743b9e9306e8cb625be8ca59aedf06e41
Ik0n <Ik0nChannel1@mail.ru>
2022-04-05 17:27:10 +0300

    Add drawerLayout

--- app/src/main/java/com/example/fsociety/MainActivity.java
+++ app/src/main/java/com/example/fsociety/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.fsociety;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBarInit();
+
     }
+
+    private void toolBarInit() {
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        toggle.setHomeAsUpIndicator(R.drawable.avatar);
+
+        toggle.syncState();
+
+    }
+
+
 }
